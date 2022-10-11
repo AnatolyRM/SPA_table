839c282b8e6f00c3a10b21fac975e9fd0a0d3d0a
AnatolyRM <1neytron@gmail.com>
2022-10-11 23:15:35 +0300

    feat: add filter

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <script setup>
-import {ref, computed} from 'vue'
+import {ref, computed, watch} from 'vue'
 import dates from './date'
 
 let userPage = ref(2)
 let thisPageNum = ref(1)
+const d = ref(dates)
 
-const pages = computed(() => Math.ceil(dates.length / userPage.value))
+const pages = computed(() => Math.ceil(d.value.length / userPage.value))
 const paginatUser = computed(() => {
   let from = (thisPageNum.value - 1) * userPage.value
   let to = from + userPage.value
-  return dates.slice(from, to)
+  return d.value.slice(from, to)
 })
 
-const pageClick = page => {
-  thisPageNum.value = page
+const pageClick = page => thisPageNum.value = page
+
+const sortByName = () => d.value.sort((a, b) => a.name.localeCompare(b.name))
+const sortByPhone = () => d.value.sort((a, b) => {
+  let numA = String(a.phone).replace(/[^0-9]/g, '')
+  let numB = String(b.phone).replace(/[^0-9]/g, '')
+  return numA - numB
+})
+const sortByUsername = () => d.value.sort((a, b) => a.username.localeCompare(b.username))
+const sortByEmail = () => d.value.sort((a, b) => a.email.localeCompare(b.email))
+const filteredList = computed(() => {
+  return d.value.filter((elem) => comp === '' ? true : elem.company.indexOf(comp) > -1)
+})
+
+const filter = () => {
+  console.log('fil')
 }
+
 </script>
 
 <template>
+  <div class="menu__filter">
+    <select id="filter-name" onChange="filter()">
+      <option value="name">Name</option>
+      <option value="phone">Phone</option>
+      <option value="username">Username</option>
+      <option value="email">Email</option>
+    </select>
+    <select id="filter-conditions" onChange="filter()">
+      <option value="equals">Равно</option>
+      <option value="contains">Содержит</option>
+      <option value="more">Больше</option>
+      <option value="less">Меньше</option>
+    </select>
+    <input type="text" id="filter-value">
+  </div>
   <div class="header">
-    <p>Name</p>
-    <p>Phone</p>
-    <p>Username</p>
-    <p>Email</p>
+    <p @click="sortByName">
+      <span class="mane-filter icon">↕</span>
+      Name
+    </p>
+    <p @click="sortByPhone">
+      <span class="phone-filter icon">↕</span>
+      Phone
+    </p>
+    <p @click="sortByUsername">
+      <span class="username-filter icon">↕</span>
+      Username
+    </p>
+    <p @click="sortByEmail">
+      <span class="email-filter icon">↕</span>
+      Email
+    </p>
   </div>
   <div class="body">
     <div class="row"
@@ -55,6 +98,12 @@ const pageClick = page => {
 .header p {
   flex-basis: 25%;
   text-align: left;
+  padding-left: 10px;
+}
+
+.header p:hover {
+  cursor: pointer;
+  background-color: #b6b5b5;
 }
 
 .row {
@@ -86,5 +135,10 @@ const pageClick = page => {
   background: #b6b5b5;
   cursor: pointer;
   color: #fff;
+}
+
+.icon {
+  margin-right: 5px;
+
 }
 </style>
